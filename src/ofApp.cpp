a83dce2717e433e58b7110be95f6eb4efb063a5a
MariPM2 <mariana.perez5@upr.edu>
2021-09-20 21:00:33 -0400

    Changed Background Color

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -47,7 +47,7 @@ void ofApp::update(){
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-    ofBackgroundGradient(ofColor(60,60,60), ofColor(10,10,10));
+    ofBackgroundGradient(ofColor(0,255,255), ofColor(0,0,128));
 
 	for(unsigned int i = 0; i < p.size(); i++){
 		p[i].draw();
